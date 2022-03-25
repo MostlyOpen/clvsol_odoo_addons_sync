824ae413142b7f125da39424f52558ff62d24048
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2022-03-25 16:53:17 -0300

    [IMP] clv_external_sync (IMP models/external_sync.py)

--- clv_external_sync/models/external_sync.py
+++ clv_external_sync/models/external_sync.py
@@ -2,7 +2,19 @@
 # Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+import logging
+from datetime import datetime
+from functools import reduce
+from ast import literal_eval
+
 from odoo import api, fields, models
+
+_logger = logging.getLogger(__name__)
+
+
+def secondsToStr(t):
+
+    return "%d:%02d:%02d.%03d" % reduce(lambda ll, b: divmod(ll[0], b) + ll[1:], [(t * 1000,), 1000, 60, 60])
 
 
 class ExternalSync(models.Model):

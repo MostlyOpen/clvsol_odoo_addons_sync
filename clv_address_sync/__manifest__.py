480577e49fab2ab3a658c0c45c89f651251c11c2
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2022-09-13 10:22:04 -0300

    [ADD] clv_address_sync

---
+++ clv_address_sync/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Address External Sync',
+    'summary': 'Address External Sync Module used by CLVsol Solutions.',
+    'version': '15.0.6.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'CLVsol Solutions',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'depends': [
+        'clv_address',
+        'clv_external_sync',
+    ],
+    'data': [
+        'data/address_category_sync.xml',
+        'data/address_marker_sync.xml',
+        'data/address_sync.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}

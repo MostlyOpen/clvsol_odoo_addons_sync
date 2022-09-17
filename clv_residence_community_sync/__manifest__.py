55767a84664b77759edc8611bfb5e885c715844b
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2022-09-16 22:15:18 -0300

    [ADD] clv_residence_community_sync

---
+++ clv_residence_community_sync/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Residence External Sync',
+    'summary': 'Residence External Sync Module used by CLVsol Solution.',
+    'version': '15.0.6.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'CLVsol Solutions',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'depends': [
+        'clv_residence_sync',
+        'clv_address_sync',
+    ],
+    'data': [
+        'data/residence_1_inc.xml',
+        'data/residence_2_sync.xml',
+        'data/address_sync.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}

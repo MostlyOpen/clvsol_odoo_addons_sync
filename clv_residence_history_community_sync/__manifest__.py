4bc4303769c8ad7bfd01d9fc8a4f514656a6ea1d
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2022-09-16 22:16:08 -0300

    [ADD] clv_residence_history_community_sync

---
+++ clv_residence_history_community_sync/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Residence History External Sync',
+    'summary': 'Residence History External Sync Module used by CLVsol Solution.',
+    'version': '15.0.6.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'CLVsol Solutions',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'depends': [
+        'clv_residence_history_sync',
+        'clv_address_history_sync',
+    ],
+    'data': [
+        'data/residence_history_1_inc.xml',
+        'data/residence_history_2_sync.xml',
+        'data/address_history_sync.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
